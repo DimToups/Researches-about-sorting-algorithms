21ca81bd97c833e05f372e7a4b65c8c5ad0bee45
Dim_Toups <dimitri.toupense@etu.unistra.fr>
2024-01-30 15:38:34 +0100

    [Java] Modification du Main pour qu'il prenne en compte les arguments

--- Java/p41-Java/src/Main.java
+++ Java/p41-Java/src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
+        // Vérification de la conformité des paramètres
 
+
+        // Décision du nombre de valeurs
+        int nbValues = 100;
+        if(args.length == 2)
+            nbValues = Integer.parseInt(args[1]);
+
+        //Décision du tri à faire
+        int tri = 0;
+        if(args.length != 0){
+            if(args[0].equals("--merge") || args[0].equals("-m"))
+                tri = 1;
+            else if(args[0].equals("--quick") || args[0].equals("-q"))
+                tri = 2;
+        }
+
+        Sorter sorter = new Sorter(nbValues);
+
+        if(tri == 0)
+            sorter.insertSort();
+        else if(tri == 1){
+            sorter.randomFill(nbValues);
+            sorter.mergeSort(0,sorter.getArray().size());
+        }
+        else{
+            sorter.randomFill(nbValues);
+            sorter.quickSort();
+        }
     }
 }

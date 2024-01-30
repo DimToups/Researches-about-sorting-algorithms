463607e79cc0cdc7d2eda4fbfc6712b08e92a6f3
Dim_Toups <dimitri.toupense@etu.unistra.fr>
2024-01-30 15:03:07 +0100

    [Java] Mise en place de la structure de la classe Sorter

---
+++ Java/p41-Java/src/Sorter.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sorter {
+    private List<Integer> array;
+    public Sorter(int arraySize){
+        this.array = new ArrayList<>(arraySize);
+    }
+    public void randomFill(){
+
+    }
+    public void displayArray(){
+
+    }
+    public void insertSort(){
+
+    }
+    public void mergeSort(int p, int q) {
+
+    }
+    public void subMergeSort(int p, int q, int r) {
+
+    }
+    public void quickSort(){
+
+    }
+    public void subQuickSort(int p, int r){
+
+    }
+    public int partition(int p, int r){
+        return 0;
+    }
+    public List<Integer> getArray(){
+        return this.array;
+    }
+    public void setArray(List<Integer> array){
+        this.array = array;
+    }
+}
